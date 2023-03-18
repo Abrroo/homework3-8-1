65d08e1e3e7058e8d62c0875800e976627468f3e
Ersyn <akhmetov.ersyn@gmail.com>
2023-03-18 19:56:47 +0300

    gg

--- homework3-8-1/homework3-8-1.cpp
+++ homework3-8-1/homework3-8-1.cpp
@@ -2,10 +2,44 @@
 //
 
 #include <iostream>
+#include <string>
+
+class forbidden_length_exception : public std::exception
+{
+public:
+    const char* what() const override
+    {
+        return "Вы ввели слово запретной длины! До свидания";
+    }
+};
 
 int function(std::string str, int forbidden_length)
+{
+    if (str.size() == forbidden_length) throw forbidden_length_exception();
+    return str.size();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+
+    int forbidden_length;
+    std::string str;
+    std::cout << "Введите запретную длину :";
+    std::cin >> forbidden_length;
+
+    try
+    {
+        while (true)
+        {
+            std::cout << "Введите слово: ";
+            std::cin >> str;
+            std::cout << "Длина слова \"" << str << "\" равна " << function(str, forbidden_length) << std::endl;
+        }
+    }
+    catch (const forbidden_length_exception& ex)
+    {
+        std::cout << ex.what();
+    }
+
 }
