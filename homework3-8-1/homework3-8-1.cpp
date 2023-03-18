4733bf03718e66e5d7ac49b53c5e3fa11caab9a2
Ersyn <akhmetov.ersyn@gmail.com>
2023-03-18 19:18:22 +0300

    Добавьте файлы проекта.

---
+++ homework3-8-1/homework3-8-1.cpp
@@ -0,0 +1,11 @@
+﻿// homework3-8-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int function(std::string str, int forbidden_length)
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
